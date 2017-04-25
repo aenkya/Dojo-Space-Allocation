a0b7ad3fb6e90c1a43b1bd2b2243ca80809c8d20
unknown <brucebigirwenkya@gmail.com>
2017-04-25 19:43:11 +0300

    Add Test for Fellow Class create test_fellow_class.py test fellow: assertIsInstance of Fellow Class

---
+++ fellow/test_fellow_class.py
@@ -0,0 +1,16 @@
+"""
+Unit tests for the fellow Class
+"""
+import unittest
+from fellow import Fellow
+
+
+class fellow_classTest(unittest.TestCase):
+    """Testing fellow_class"""
+
+    def setUp(self):
+        self.fellow = Fellow()
+
+    def test_fellow_instance(self):
+        self.assertIsInstance(
+            self.fellow, Fellow, msg='The object should be an instance of the `Fellow` class')
