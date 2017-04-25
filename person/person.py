747e17167ab18cc930dfad2aca4d1c8745c7165c
unknown <brucebigirwenkya@gmail.com>
2017-04-25 19:51:22 +0300

    Add Person Class Initialize variables from arguments for age, gender, nationality, id and name

---
+++ person/person.py
@@ -0,0 +1,8 @@
+class Person(object):
+
+    def __init__(self, id, name, gender, age, nationality):
+        self.id = id
+        self.name = name
+        self.gender = gender
+        self.age = age
+        self.nationality = nationality
