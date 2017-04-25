4c53318806a0924595d7ac5f79fdb63afb550877
unknown <brucebigirwenkya@gmail.com>
2017-04-25 18:57:19 +0300

    Add Tests for Living Space class Check that living_space is instantiated

---
+++ living_space/test_living_space_class.py
@@ -0,0 +1,20 @@
+"""
+Unit tests for the living_space Class
+"""
+import unittest
+from living_space import living_space
+
+
+class living_space_classTest(unittest.TestCase):
+    """Testing living_space_class"""
+
+    def setUp(self):
+        self.living_space = living_space()
+
+    def test_living_space_instance(self):
+        self.assertIsInstance(
+            self.living_space, living_space, msg='The object should be an instance of the `living_space` class')
+
+    def test_create_living_space_returns_value_error_if_arg_not_list():
+        self.assertRaises(
+            ValueError, self.living_space.create_living_space(), 'two')
