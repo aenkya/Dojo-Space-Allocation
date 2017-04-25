d50e5df6974771d0e85881a0b7fb5d020447bcc3
unknown <brucebigirwenkya@gmail.com>
2017-04-25 19:20:37 +0300

    Add Class for Office Space initialize the capacity to 6 people

---
+++ office_space/office_space.py
@@ -0,0 +1,9 @@
+from room import Room
+
+
+class office_space(Room):
+    """office space class"""
+
+    def __init__(self):
+        super(office_space, self).__init__()
+        self.capacity = 6
