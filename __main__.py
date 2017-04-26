34441171fdb8fe2365f4ea0beb67f9de669d7668
unknown <brucebigirwenkya@gmail.com>
2017-04-26 18:52:08 +0300

    Add Decorators to Docopt Implementation The get_docopt_args decorator provides arguments from the program input script and passes it on to the function that is called

--- __main__.py
+++ __main__.py
@@ -16,26 +16,61 @@ Examples
 """
 
 import sys
+import cmd
 from docopt import docopt, DocoptExit
 from dojo.dojo import Dojo
 
+dojo = Dojo('DOJO01', 'The Dojo')
 
-class main(object):
+
+def get_docopt_args(func):
+    """
+    Decorator to pass the results of the docscript command to the function
+    """
+
+    def func_wrapper(self, arg):
+        try:
+            opt = docopt(func_wrapper.__doc__, arg)
+        except DocoptExit as e:
+            return
+        except SystemExit:
+            return
+
+        return func(self, opt)
+    func_wrapper.__doc__ = func.__doc__
+    return func_wrapper
+
+
+class SpaceAllocation(cmd.Cmd):
     """Main Application Entry Point"""
+    print("\nWelcome to the Dojo Space Allocation Program")
+    print("______________________________________________")
+    print('\nLocation Name: ' + dojo.name + '')
 
-    def __init__(self):
-        print("\nWelcome to the Dojo Space Allocation Program")
-        print("______________________________________________")
-        self.dojo = Dojo('DOJO01', 'The Dojo')
-        print('\nLocation Name: ' + self.dojo.name + '')
+    prompt = 'SpaceAlloc:  '  # prompt to replace default cmd prompt message
 
-    def create_room(self, room_type, room_names):
-        self.dojo.create_room(str(room_type), room_names)
+    @get_docopt_args
+    def do_create_room(self, params):
+        """ Usage: create_room <room_type> <room_name>..."""
+
+        return self.dojo.create_room(params["<room_type"], params["<room_name>"])
+
+    # def add_person(self, person_name, person_type, wants_accomodation):
+    # return self.dojo.add_person(str(person_name), person_type,
+    # wants_accomodation)
 
 
-if __name__ == '__main__':
-    opt = docopt(__doc__)
-    dojo = main()
+SpaceAllocation().cmdloop()
 
-    """Usage: create_room <room_type> <room_name>..."""
-    dojo.create_room(opt["<room_type>"], opt["<room_name>"])
+# Old Implementation
+# if __name__ == '__main__':
+#     opt = docopt(__doc__)
+#     dojo = main()
+
+#     """Usage: create_room <room_type> <room_name>..."""
+#     try:
+#         room_type = opt["<room_type>"]
+#         room_names = opt["<room_name>"]
+#         dojo.create_room(room_type, room_names)
+
+#     except: ValueError
