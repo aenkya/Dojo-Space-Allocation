3d9402a17e20c77846450f1a53aa15a694e66a15
unknown <brucebigirwenkya@gmail.com>
2017-04-27 20:19:14 +0300

    Rellocate unit tests from test_main.py

--- test_main.py
+++ test_main.py
@@ -2,24 +2,14 @@
 Unit tests for the __main__.py file
 """
 import unittest
-from __main__ import main
 from dojo.dojo import Dojo
+from room.room import Room
+from living_space.living_space import living_space
+from office_space.office_space import office_space
 
 
 class test_main(unittest.TestCase):
     """Testing Main Class"""
 
     def setUp(self):
-        self.main = main()
-
-    def test_main_instance(self):
-        self.assertIsInstance(
-            self.main, main, msg='The object should be an instance of the `main` class')
-
-    def test_create_room_function_creates_rooms_for_each_room_name_provided(self):
-        self.main.create_room('Office', ['Green', 'Yellow'])
-        self.assertEqual(2, len(self.main.dojo.office_spaces))
-
-    def test_create_room_function_creates_office_space_if_room_type_is_Office(self):
-        self.assertIsInstance(self.main.create_room('Office', [
-                              'Green']), Room, msg='The object should return an instance of office_space Class')
+        self.dojo = Dojo('DOJO001', 'The Dojo')
