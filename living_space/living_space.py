ce7fb4b141c27860ea525dd89d9bdc9079c32acc
unknown <brucebigirwenkya@gmail.com>
2017-04-27 18:03:07 +0300

    Add populate slots method Adds slots to the room based on the room's capacity

--- living_space/living_space.py
+++ living_space/living_space.py
@@ -7,3 +7,8 @@ class living_space(Room):
     def __init__(self, name):
         super(living_space, self).__init__(name)
         self.capacity = 4
+        self.populate_slots()
+
+    def populate_slots(self):
+        for i in range(0, self.capacity):
+            self.room_mates.append('X')
