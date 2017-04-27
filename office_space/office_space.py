27a0c57acd403ddf3bade7e60d8ac7b8e71d269f
unknown <brucebigirwenkya@gmail.com>
2017-04-27 18:02:08 +0300

    Add populate_slots method This function populates the room with slots for occupation Relies on the room capacity

--- office_space/office_space.py
+++ office_space/office_space.py
@@ -6,4 +6,8 @@ class office_space(Room):
 
     def __init__(self, name):
         super(office_space, self).__init__(name)
-        self.capacity = 6
+        self.populate_slots()
+
+    def populate_slots(self):
+        for i in range(0, self.capacity):
+            self.room_mates.append('X')
