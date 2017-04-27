22620bab8d02ee4f63f001033f79fdc351520129
unknown <brucebigirwenkya@gmail.com>
2017-04-27 13:56:39 +0300

    Fix docopt argument input Use square brackets to provide optional input for wants_accomodation

--- __main__.py
+++ __main__.py
@@ -3,19 +3,22 @@ Dojo Space Allocation
 
 Usage:
     dojo create_room <room_type> <room_name>...
-    dojo add_person <person_name> <FELLOW|STAFF> [wants_accomodation]
+    dojo add_person <person_name> <person_type> [wants_accomodation]
     dojo -h | --help
     dojo --version
+    dojo close
 
 Options:
     -h --help  Show this screen
     --version  Show version
+    --close -c   Close the program
 
 Examples
     dojo create_room Office LLK
 """
 
 import sys
+import os
 import cmd
 from docopt import docopt, DocoptExit
 from dojo.dojo import Dojo
@@ -36,13 +39,16 @@ def get_docopt_args(func):
         except SystemExit:
             return
 
-        return func(self, opt)
+        return func(opt)
+    func_wrapper.__name__ = func.__name__
     func_wrapper.__doc__ = func.__doc__
+    func_wrapper.__dict__.update(func.__dict__)
     return func_wrapper
 
 
 class SpaceAllocation(cmd.Cmd):
     """Main Application Entry Point"""
+    os.system('cls')
     print("\nWelcome to the Dojo Space Allocation Program")
     print("______________________________________________")
     print('\nLocation Name: ' + dojo.name + '')
@@ -50,14 +56,19 @@ class SpaceAllocation(cmd.Cmd):
     prompt = 'SpaceAlloc:  '  # prompt to replace default cmd prompt message
 
     @get_docopt_args
-    def do_create_room(self, params):
+    def do_create_room(params):
         """ Usage: create_room <room_type> <room_name>..."""
-
         return dojo.create_room(params["<room_type>"], params["<room_name>"])
 
-    # def add_person(self, person_name, person_type, wants_accomodation):
-    # return self.dojo.add_person(str(person_name), person_type,
-    # wants_accomodation)
+    @get_docopt_args
+    def do_add_person(params):
+        """ Usage: add_person <person_name> <person_type> [<wants_accomodation>]"""
+        wants_accomodation = params["<wants_accomodation>"]
+        if wants_accomodation == 'Y':
+            wants_accomodation = True
+        else:
+            wants_accomodation = False
+        return dojo.add_person(params["<person_name>"], params["<person_type>"], wants_accomodation)
 
 
 SpaceAllocation().cmdloop()
