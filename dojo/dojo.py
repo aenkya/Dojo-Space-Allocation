a1cbc705750a04232265cb2589700e0d9130ff04
unknown <brucebigirwenkya@gmail.com>
2017-04-27 11:21:16 +0300

    Add Check for existing room name Function to check if the room name passed in already exists for that dictionary

--- dojo/dojo.py
+++ dojo/dojo.py
@@ -17,6 +17,8 @@ class Dojo(object):
         self.staff = {}
         self.living_spaces = {}
         self.office_spaces = {}
+        self.available_living_slots = {}
+        self.available_office_slots = {}
 
     def create_room(self, room_type, rooms):
         if isinstance(rooms, list):
@@ -25,17 +27,25 @@ class Dojo(object):
                 raise ValueError("Space Type not recognized")
             for room_name in rooms:  # Loop through rooms argument for all rooms in list
                 if space == 'Office':
-                    import pdb
-                    # pdb.set_trace()
-                    current_room = office_space(room_name)
-                    self.living_spaces.append(current_room)
-                    print("An Office Called " + current_room.name +
-                          " has been successfully created!")
+                    if self.check_if_room_exists(room_name, space) is True:
+                        print("Room with name " +
+                              room_name + " Already Exists!")
+                        continue
+                    else:
+                        current_room = office_space(room_name)
+                        self.office_spaces[room_name] = current_room
+                        print("An Office Called " + current_room.name +
+                              " has been successfully created!")
                 else:
-                    current_room = living_space(room_name)
-                    self.office_spaces.append(current_room)
-                    print("A Living Space Called " + current_room.name +
-                          " has been successfully created!")
+                    if self.check_if_room_exists(room_name, space) is True:
+                        print("Room with name " +
+                              room_name + " Already Exists!")
+                        continue
+                    else:
+                        current_room = living_space(room_name)
+                        self.living_spaces[room_name] = current_room
+                        print("A Living Space Called " + current_room.name +
+                              " has been successfully created!")
         else:
             raise ValueError('Rooms argument should be a list')
 
@@ -53,15 +63,16 @@ class Dojo(object):
             import pdb
             pdb.set_trace()
         else:
-            if self.check_if_exists(person_name, person_type) is True:
-                print ("Person Exists!")
+            if self.check_if_person_exists(person_name, person_type) is True:
+                print("Person Exists!")
                 return
             else:
                 fellow = Fellow(person_name, gender, age, nationality)
                 self.fellows[person_name] = fellow
                 print(person_name + ' has been successfully added')
+                self.add_to_room(person_name)
 
-    def check_if_exists(self, person, person_type):
+    def check_if_person_exists(self, person, person_type):
         """Function to check if user has already been added"""
         if person_type == 'Fellow':
             if person in self.fellows:
@@ -76,3 +87,50 @@ class Dojo(object):
                 return False
         else:
             return False
+
+    def check_if_room_exists(self, room, room_type):
+        """Function to check if user has already been added"""
+        if room_type == 'Living':
+            if room in self.living_spaces:
+                return True
+            else:
+                return False
+        elif room_type == 'Office':
+            if room in self.office_spaces:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def add_to_room(self, person):
+        self.find_empty_slots('office_space')
+        # for i in self.office_spaces:
+        #     print (self.office_spaces[i].name)
+        # if isinstance(self.fellows[person], Fellow):
+
+    def find_empty_slots(self, room_type):
+        if room_type == 'living_space':
+            for i in self.living_spaces:
+                for n in range(0, len(self.living_spaces[i].room_mates)):
+                    if self.living_spaces[i].room_mates[n] == 'X':
+                        if self.living_spaces[i].name in self.available_living_slots:
+                            self.available_living_slots[
+                                self.living_spaces[i].name] += 1
+                        else:
+                            self.available_living_slots[
+                                self.living_spaces[i].name] = 1
+        if room_type == 'office_space':
+            for i in self.office_spaces:
+                for n in range(0, len(self.office_spaces[i].room_mates)):
+                    if self.office_spaces[i].room_mates[n] == 'X':
+                        if self.office_spaces[i].name in self.available_office_slots:
+                            self.available_office_slots[
+                                self.office_spaces[i].name] += 1
+                        else:
+                            self.available_office_slots[
+                                self.office_spaces[i].name] = 1
+
+    def show_available_room(self, room_name, room_type):
+        if room_type == 'living_space':
+            print(self.living_spaces.find('room_name'))
