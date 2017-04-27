7f4b6a46b5b00e174561a44388e311aadeb49ac9
unknown <brucebigirwenkya@gmail.com>
2017-04-27 11:49:46 +0300

    Add request_accomodation_function Function to set the wants_accomodation boolean to true

--- fellow/fellow.py
+++ fellow/fellow.py
@@ -4,6 +4,10 @@ from person.person import Person
 class Fellow(Person):
     """Fellow class"""
 
-    def __init__(self):
-        super(Fellow, self).__init__(id, name, gender, age, nationality)
+    def __init__(self, name, gender, age, nationality):
+        super(Fellow, self).__init__(self, name, gender, age, nationality)
         self.role = 'Fellow'
+        self.wants_accomodation = False
+
+    def request_accomodation(self):
+    	self.wants_accomodation = True
